cfe13160ceb63e5188b3f00d6d9ad9133d8b29fc
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-31 12:26:31 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Fragment>
+			<header>
+				<h1>Comment on utilise une API en JavaScript Vanilla</h1>
+			</header>
+			<main>
+				<section class="explain">
+					<p>
+						Nous allons voir le fonctionnement d'une API en
+						JavaScript Vanilla.
+					</p>
+					<p>
+						Comment ? En utilisant
+						<a
+							href="https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch"
+							target="_blank"
+						>
+							<code>fetch</code>
+						</a>
+					</p>
+				</section>
+				<section class="container">
+					<h1>Une blague sur Chuck Norris ?</h1>
+					<div>
+						<div class="render">
+							<span class="quote">
+								Jesus shamelessly wore a Chuck Norris beard
+							</span>
+						</div>
+						<button>Une autre blague</button>
+					</div>
+				</section>
+			</main>
+		</Fragment>
+	);
+};
 
 export default App;
